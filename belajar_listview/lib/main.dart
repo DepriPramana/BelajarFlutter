be6c6dd83a5edf661a71a494d68500810c206db6
I Gede Depri Pramana <46771648+DepriPramana@users.noreply.github.com>
2020-08-27 15:27:24 +0800

    Gesture Detection

--- belajar_listview/lib/main.dart
+++ belajar_listview/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,49 +11,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Widget> widgets = [];
-  int counter = 1;
-
+  Random random = Random();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Belajar List View"),
+          title: Text("Belajar Animate Gesture"),
         ),
-        body: ListView(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                RaisedButton(
-                    child: Text("Tambah Data"),
-                    onPressed: () {
-                      setState(() {
-                        widgets.add(Text(
-                          "List ke: " + counter.toString(),
-                          style: TextStyle(fontSize: 35),
-                        ));
-                        counter++;
-                      });
-                    }),
-                RaisedButton(
-                    child: Text("Hapus Data"),
-                    onPressed: () {
-                      setState(() {
-                        if (counter > 1) {
-                          widgets.removeLast();
-                          counter--;
-                        }
-                      });
-                    })
-              ],
+        body: Center(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {});
+            },
+            child: AnimatedContainer(
+              duration: Duration(seconds: 1),
+              color: Color.fromARGB(255, random.nextInt(256),
+                  random.nextInt(256), random.nextInt(256)),
+              width: 50.0 + random.nextInt(101),
+              height: 50.0 + random.nextInt(101),
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: widgets,
-            )
-          ],
+          ),
         ),
       ),
     );

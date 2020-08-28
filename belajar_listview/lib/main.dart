4e73fe243888d779e0ebbb24f38ead7c98f41233
I Gede Depri Pramana <46771648+DepriPramana@users.noreply.github.com>
2020-08-28 16:51:47 +0800

    Belajar Navigation

--- belajar_listview/lib/main.dart
+++ belajar_listview/lib/main.dart
@@ -1,148 +1,15 @@
-import 'dart:math';
+import 'package:belajar_listview/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final Random _random = Random();
-
-  Color _color = Color(0xFFFFFFFF);
-  Color color2 = Color(0xFFFF0000);
-  void changeColor() {
-    setState(() {
-      _color = Color.fromARGB(
-        //or with fromRGBO with fourth arg as _random.nextDouble(),
-        _random.nextInt(256),
-        _random.nextInt(256),
-        _random.nextInt(256),
-        _random.nextInt(256),
-      );
-      color2 = Color.fromARGB(
-        //or with fromRGBO with fourth arg as _random.nextDouble(),
-        _random.nextInt(253),
-        _random.nextInt(256),
-        _random.nextInt(256),
-        _random.nextInt(256),
-      );
-    });
-  }
-
-  bool isAccepted = false;
-  Color targetColor;
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Belajar Drag & Drop"),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Draggable<Color>(
-                  data: _color,
-                  child: SizedBox(
-                    width: 50,
-                    height: 50,
-                    child: Material(
-                      color: _color,
-                      shape: StadiumBorder(),
-                      elevation: 3,
-                    ),
-                  ),
-                  childWhenDragging: SizedBox(
-                    width: 50,
-                    height: 50,
-                    child: Material(
-                      color: Colors.grey.withOpacity(0.7),
-                      shape: StadiumBorder(),
-                      elevation: 3,
-                    ),
-                  ),
-                  feedback: SizedBox(
-                    width: 50,
-                    height: 50,
-                    child: Material(
-                      color: _color.withOpacity(0.8),
-                      shape: StadiumBorder(),
-                      elevation: 3,
-                    ),
-                  ),
-                ),
-                Draggable<Color>(
-                  data: color2,
-                  child: SizedBox(
-                    width: 50,
-                    height: 50,
-                    child: Material(
-                      color: color2,
-                      shape: StadiumBorder(),
-                      elevation: 3,
-                    ),
-                  ),
-                  childWhenDragging: SizedBox(
-                    width: 50,
-                    height: 50,
-                    child: Material(
-                      color: Colors.grey.withOpacity(0.7),
-                      shape: StadiumBorder(),
-                      elevation: 3,
-                    ),
-                  ),
-                  feedback: SizedBox(
-                    width: 50,
-                    height: 50,
-                    child: Material(
-                      color: color2.withOpacity(0.8),
-                      shape: StadiumBorder(),
-                      elevation: 3,
-                    ),
-                  ),
-                )
-              ],
-            ),
-            DragTarget<Color>(
-              onWillAccept: (value) => true,
-              onAccept: (value) {
-                isAccepted = true;
-                targetColor = value;
-                changeColor();
-              },
-              builder: (context, candidateData, rejectedData) {
-                return (isAccepted)
-                    ? SizedBox(
-                        width: 100,
-                        height: 100,
-                        child: Material(
-                          color: targetColor,
-                          shape: StadiumBorder(),
-                          elevation: 0,
-                        ),
-                      )
-                    : SizedBox(
-                        width: 100,
-                        height: 100,
-                        child: Material(
-                          color: Colors.black26,
-                          shape: StadiumBorder(),
-                          elevation: 0,
-                        ),
-                      );
-              },
-            )
-          ],
-        ),
-      ),
+      home: LoginPage(),
     );
   }
 }
---
+++ belajar_listview/lib/main_page.dart
@@ -0,0 +1,23 @@
+import 'package:belajar_listview/second_page.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Page"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Go to second screen"),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return SecondPage();
+            }));
+          },
+        ),
+      ),
+    );
+  }
+}

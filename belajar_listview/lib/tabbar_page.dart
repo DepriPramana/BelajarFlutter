e29b0d629a92fa2b32f80ac23a7f9079a2de4b65
I Gede Depri Pramana <46771648+DepriPramana@users.noreply.github.com>
2020-09-02 10:44:54 +0800

    Belajar Tabbar Custom

--- belajar_listview/lib/tabbar_page.dart
+++ belajar_listview/lib/tabbar_page.dart
@@ -3,38 +3,34 @@ import 'package:flutter/material.dart';
 class TabBarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var tabBar = TabBar(
+      indicator: BoxDecoration(
+        color: Colors.green,
+        border: Border(
+          top: BorderSide(color: Colors.white),
+        ),
+      ),
+      tabs: <Widget>[
+        Tab(
+          icon: Icon(Icons.access_alarm),
+          text: "Alarm",
+        ),
+        Tab(
+          icon: Icon(Icons.computer),
+          text: "Computer",
+        ),
+      ],
+    );
     return DefaultTabController(
-      length: 4,
+      length: 2,
       child: Scaffold(
         appBar: AppBar(
           title: Text("Belajar TabBar"),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.access_alarm),
-                text: "Alarm",
-              ),
-              Tab(
-                child: Image(
-                  image: AssetImage("assets/facebook.png"),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Tab(
-                icon: Icon(Icons.adb),
-                text: '',
-              ),
-              Tab(
-                text: "News",
-              )
-            ],
-          ),
+          bottom: tabBar,
         ),
         body: TabBarView(children: <Widget>[
           Center(child: Text("Tab 1")),
           Center(child: Text("Tab 2")),
-          Center(child: Text("Tab 3")),
-          Center(child: Text("Tab 4")),
         ]),
       ),
     );
